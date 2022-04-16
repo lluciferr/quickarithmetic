5b9de258ea82618ff179aad468e6de524f344f60
lluciferr <82355776+lluciferr@users.noreply.github.com>
2022-04-16 12:33:09 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+const op1 = document.querySelector("#operand1")
+const op2 = document.querySelector("#operand2")
+const res1 = document.querySelector("#ran1")
+const res2 = document.querySelector("#ran2")
+const operator = document.querySelector("#operator")
+const qn1 = document.querySelector("#qn1")
+const qn2 = document.querySelector("#qn2")
+const ansbydev = document.querySelector("#ansauto")
+const userans = document.querySelector("#userans")
+const time = document.querySelector("#time")
+const displayedOperator = document.querySelector("#displayedOperator")
+const back = document.querySelector("#back")
+
+const ope = document.querySelectorAll(".ope")
+const qn = document.querySelectorAll(".qn")
+
+var ansact;
+
+var startTime
+var endTime
+var timeDiff
+
+const cng =()=>{
+    res1.innerText = op1.value
+    res2.innerText = op2.value
+
+}
+
+
+ope.forEach(element => {
+    element.addEventListener("change",cng)
+    
+});
+
+cng()
+
+qn.forEach(ele=>{
+    ele.style.display = "inline"
+   
+})
+
+const qnsgen=()=>{
+    qn1.innerText = Math.floor( Math.random() * Math.pow(10,Number( op1.value)))
+    qn2.innerText = Math.floor( Math.random() * Math.pow(10,Number(op2.value)))
+    findans()
+    ansbyuser =0
+    userans.innerText=""
+    userans.classList.remove("green")
+
+    // check.innerText =""
+	startTime = Date.now()
+    time.innerText = "time"
+}
+
+const ans =()=>{
+    console.log( qn1.innerText + qn2.innerHTML)
+}
+
+const findans= ()=>{
+    console.log(operator.value);
+    displayedOperator.innerText = operator.value
+    switch (operator.value) {
+        case "add":
+            ansact =Number( qn1.innerText) + Number( qn2.innerHTML)
+            break;
+    	case "sub":
+            ansact =Number( qn1.innerText) - Number( qn2.innerHTML)
+            break;
+        case "mul":
+            ansact = Number(qn1.innerText) * Number( qn2.innerHTML)
+            break;
+        case "div":
+            // ansact =Number( qn1.innerText) / Number( qn2.innerHTML)
+            // for ease result is in integer
+            ansact =parseInt( Number( qn1.innerText) / Number( qn2.innerHTML))
+            break;
+        default:
+            ansact ="error due to ..."
+            break;
+    }
+
+    // ansbydev.innerText = Math.abs( ansact)
+}
+
+operator.addEventListener('change',findans)
+
+const btn = document.querySelectorAll(".btn")
+
+var ansbyuser =  0
+
+const handleinput=(element)=>{
+    userans.classList.remove("green")
+	var digit= element.target.value
+    ansbyuser = ansbyuser * 10  + Number( digit)
+    userans.innerText = ansbyuser
+    ansCheck()
+
+}
+
+btn.forEach(element => {
+    element.addEventListener('click',handleinput)
+});
+
+// const check = document.querySelector("#check")
+
+const ansCheck=()=>{
+    if (ansbyuser == Math.abs( ansact)) {
+        // check.innerText =" milooo"
+        // ans check and give feedback
+        userans.classList.add("green")
+        endTime = Date.now()
+        timeDiff = Math.round((endTime - startTime )/100)/10  
+        console.log(timeDiff);
+        time.innerText = timeDiff + "  sec"
+
+    }
+    else{
+        userans.classList.remove("green")
+
+    }
+}
+
+const backevent =()=>{
+	ansbyuser = Math.floor( ansbyuser/ 10)
+    userans.innerText = ansbyuser
+    ansCheck() 
+    
+}
+
+back.addEventListener('click',backevent)
+
+
+qnsgen()
